5fb28912c29d1048f596aceb2462beca65f72c29
tequ <git@tequ.dev>
2024-10-03 10:16:27 +0900

    add networks

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from './networks'
+
+import { batchTestnet, devnet, mainnet, testnet, xahau, xahauTestnet } from './networks'
+export const networks = [mainnet, testnet, devnet, batchTestnet, xahau, xahauTestnet]
